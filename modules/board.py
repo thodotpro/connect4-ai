ba36303f3be5d3b7a06ab6ec13348bbe49b420fc
thodotpro <thodotpro@proton.me>
2025-07-24 19:35:53 +0200

    board

--- modules/board.py
+++ modules/board.py
@@ -4,16 +4,37 @@ class Board:
     def __init__(self, rows: int, columns: int)->None:
         self.rows = rows
         self.columns = columns
-        self.board = np.zeros((self.rows, self.columns), dtype=int)
+        self._board = np.zeros((self.rows, self.columns), dtype=int)
 
-    def print_board(self):
-        print(self.board)
+    def drop_piece(self, col: int)->None:
+        pass
+
+    def _is_valid_drop(self, col: int)->bool:
+        pass
+
+    def check_win(self)->bool:
+        pass
+
+    def _check_horizontal_win(self)->bool:
+        pass
+
+    def _check_vertical_win(self)->bool:
+        pass
+
+    def _check_diagonal_left_win(self)->bool:
+        pass
+
+    def _check_diagonal_right_win(self)->bool:
+        pass
+
+
+    def print_board(self)->None:
+        print(self._board)
 
 
 if __name__ == "__main__":
 
     board = Board(6, 7)
     board.print_board()
-
 
 
